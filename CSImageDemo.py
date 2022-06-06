40cdacf13913181cd32bab65fc931c86555ca833
coleharlow12 <33840536+coleharlow12@users.noreply.github.com>
2022-06-06 18:21:08 -0400

    Take FFT and plot it
    It may be easier initially if I do a cosine transform rather than an FFT

--- CSImageDemo.py
+++ CSImageDemo.py
@@ -2,6 +2,7 @@ from PIL import Image
 from PIL import ImageOps
 import numpy as np
 import os
+import matplotlib.pyplot as plt
 
 # Purpose
 
@@ -13,12 +14,31 @@ imgPath = os.path.join(cwd,"images","ManwithCam.jpg")	#Creates path to image
 im = ImageOps.grayscale(Image.open(imgPath)) 			#Converts to grayscale
 OrigIm = np.asarray(im)									#Stores data in numpy array
 
-print(OrigIm.shape)
-
 im.show()
 
 # Take FFT of the image to show its frequency composition
-OrgImFFT = np.fft.fft2(OrigIm)
+
+NX = OrigIm.shape[0]+1 #size of x after fft
+NY = OrigIm.shape[1]+1 #size of y after fft
+
+OrigImFFT = np.fft.fftshift(np.fft.fft2(OrigIm,s=[NX,NY])) 	#Takes fft and shift along all axes
+ImFFTNorm = 10*np.log10(np.absolute(OrigImFFT)/np.max(np.absolute(OrigImFFT)))
+
+xf = np.arange(start=-NX/2+1,stop=NX/2+1,step = 1)
+yf = np.arange(start=-NY/2+1,stop=NY/2+1,step = 1)
+
+xfg,yfg = np.meshgrid(xf,yf)
+print(xf)
+print(NX/2)
+print(yfg.shape)
+print(ImFFTNorm.shape)
 
 
-# Take wavelet decomposition to show its composition
+plt.pcolor(xfg,yfg,ImFFTNorm,cmap='jet');
+plt.colorbar();plt.clim(-20,0)
+plt.xlabel('cycles per aperture',fontsize=16,c='black')
+plt.ylabel('cycles per aperture',fontsize=16,c='black')
+
+plt.show()
+
+# Preparation for Sparse Recovery
